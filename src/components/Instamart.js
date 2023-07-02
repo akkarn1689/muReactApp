5f010fde734832053d82aca33e0df234e1b29c25
Ashish Karn <akkarn1689@gmail.com>
2023-07-02 14:26:28 +0530

    error-check

--- src/components/Instamart.js
+++ src/components/Instamart.js
@@ -2,7 +2,7 @@ const Instamart = () => {
     return (
         <div>
             <h1>Instamart</h1>
-            <h1>100s of component inside it</h1>
+            <h1>Ab aur Error nahi dekha jayega</h1>
         </div>
     );
 };
